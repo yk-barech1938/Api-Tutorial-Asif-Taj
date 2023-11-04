e60a4e3a991558a3f88424e936fb713152eda7bd
Younas KHan <ykbarech@gmail.com>
2023-11-04 22:01:05 +0500

    add JSON Model and API is hit in code

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'Models/PostsModel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,10 +13,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // extra work because we dont have array name in response
+  // in response it is just returning objects in array
+  List<PostsModel> postList = [];
+  // response is future, because if we hit the api end url we wait
+  // so we wait that's why we use future function
+  // async use because of future function we wait
+  Future<List<PostsModel>> getAllPostsApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+
+    // every response is coming with the its status code
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    } else {return postList;}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('API Asif Taj'),),
+      appBar: AppBar(
+        title: const Text('API Asif Taj'),
+      ),
       body: const Column(
         children: [],
       ),
